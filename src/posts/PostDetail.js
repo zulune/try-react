79e3d9b158b556c03b1dca4cbb75bef92d4713ff
zulune <mars.xaker@gmail.com>
2018-06-24 21:51:17 +0300

    Component and props

---
+++ src/posts/PostDetail.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class PostDetail extends Component {
+  constructor (props) {
+    super(props)
+    this.titleWasClicked = this.titleWasClicked.bind(this)
+  }
+  titleWasClicked (event) {
+  	event.preventDefault()
+  }
+  render () {
+  	const {post} = this.props
+    return (
+      <div>
+        <h1 onClick={this.titleWasClicked}>{post.title}</h1>
+        <p>{post.content}</p>
+      </div>
+    )
+  }
+}
+
+export default PostDetail
---
+++ src/posts/PostList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PostData from '../data/posts.json'
+import PostDetail from './PostDetail'
+
+class PostList extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Hello There</h1>
+        {PostData.map((item, index) => {
+        	return <PostDetail post={item} key={`post-list-key ${index}`} />
+        })}
+      </div>
+    )
+  }
+}
+
+export default PostList
