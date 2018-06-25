f87cd283f3462a93614550bb3531fd7e0c91087c
zulune <mars.xaker@gmail.com>
2018-06-25 20:37:40 +0300

    Toggle state

--- src/posts/PostDetail.js
+++ src/posts/PostDetail.js
@@ -4,7 +4,12 @@ class PostDetail extends Component {
   constructor (props) {
     super(props)
     this.titleWasClicked = this.titleWasClicked.bind(this)
+    this.toggleContent = this.toggleContent.bind(this)
+    this.state = {
+      showContent: true
+    }
   }
+  
   titleWasClicked (event) {
   	event.preventDefault()
   	const {dataCallback} = this.props
@@ -12,12 +17,22 @@ class PostDetail extends Component {
       dataCallback('hello world!')
   	}
   }
+
+  toggleContent(event) {
+    event.preventDefault();
+    this.setState({
+      showContent: !this.state.showContent
+    })
+  }
+
   render () {
-  	const {post} = this.props
+    const {post} = this.props
+    const {showContent} = this.state
     return (
       <div>
         <h1 onClick={this.titleWasClicked}>{post.title}</h1>
-        <p>{post.content}</p>
+        { showContent === true ? <p>{post.content}</p> : ""}
+        <button onClick={this.toggleContent}>Toggle Content Display</button>
       </div>
     )
   }
